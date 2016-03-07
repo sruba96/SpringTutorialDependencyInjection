578421bc96c4660be27da4155e09de3965ac8da1
Paweł Malina <sruba96@gmail.com>
2016-03-07 11:14:42 +0100

    I added  factory-method. Configured by xml.

--- src/main/java/spring/tutorial/Main.java
+++ src/main/java/spring/tutorial/Main.java
@@ -9,6 +9,12 @@ import spring.tutorial.domain.User;
 import spring.tutorial.implementations.LoggerImpl;
 import spring.tutorial.implementations.UserRepositoryImplementations;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
 /**
  * Created by pawel on 07.03.16.
  */
@@ -24,5 +30,13 @@ public class Main {
 
         User janek = usersRepository.createUser("Janek");
 
+
+//        Locale locale = Locale.getDefault();
+//        Calendar calendar = GregorianCalendar.getInstance(locale);
+//
+//        DateFormat dateFormatter = SimpleDateFormat.getDateInstance();
+        GregorianCalendar calendar = context.getBean("calendar" , GregorianCalendar.class);
+        DateFormat dateFormatter = context.getBean("formatter" , SimpleDateFormat.class);
+        System.out.println(dateFormatter.format(calendar.getTime()));
     }
 }
